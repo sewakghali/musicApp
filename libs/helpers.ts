490746a011a362f4bdd7ed0ba95610061ebeca95
Sewak Singh <sewaksingh2212@gmail.com>
2023-06-15 20:04:07 -0400

    updated imports for vercel

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -3,8 +3,7 @@ import { Price } from "../types";
 export const getUrl = ()=>{
    let url = 
    process.env.NEXT_PUBLIC_SITE_URL ?? 
-   process.env.NEXT_PUBLIC_VERCEL_URL ??
-   'http://localhost:3000/';
+   process.env.NEXT_PUBLIC_VERCEL_URL!!;
 
    url = url.includes('http')? url: `https://${url}`;
    url = url.charAt(url.length-1) === '/' ? url : `${url}/`;
